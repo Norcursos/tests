7818212e66a142874c2e75a877a67a1ae55366ea
Developer <dev@example.com>
2025-06-20 10:45:28 +0000

    TDD: Implementar función calcular_descuento (FASE VERDE)
    - Implementación mínima: importe - (importe * porcentaje / 100.0)
    - Todos los 17 tests pasan correctamente
    - Soporta descuentos básicos, fraccionarios, negativos y >100%
    - Funciona con importes decimales

--- descuentos.cpp
+++ descuentos.cpp
@@ -1,6 +1,5 @@
 #include "descuentos.h"
 
 double calcular_descuento(double importe, double porcentaje_descuento) {
-    // Implementación vacía - TDD: empezamos con test que falla
-    return 0.0;
+    return importe - (importe * porcentaje_descuento / 100.0);
 }
